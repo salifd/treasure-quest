512655ff75c3a2dd0c1ee6ed50706a180deb3598
Salif <salif.diop@spoon.ai>
2023-10-13 23:08:29 +0200

    Initial Commit

---
+++ TreasureQuest/TreasureQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreasureQuestApp.swift
+//  TreasureQuest
+//
+//  Created by Salif DIOP on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TreasureQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
